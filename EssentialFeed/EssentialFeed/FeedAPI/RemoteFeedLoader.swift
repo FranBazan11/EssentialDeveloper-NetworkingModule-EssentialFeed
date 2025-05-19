dd53c66e626efec0db9dbc8fcf545808c94bc330
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2025-05-19 01:42:47 -0300

    Move RemoteFeedLoader components to production

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Juan Francisco Bazan Carrizo on 19/05/2025.
+//
+
+import Foundation
+
+public class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
