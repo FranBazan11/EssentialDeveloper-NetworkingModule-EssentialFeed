de14b9c9a2708b630fe6f95445ec32a69b75f471
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2025-05-26 11:44:03 -0300

    Delivers connectivity error on client error

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -16,11 +16,17 @@ public class RemoteFeedLoader {
         self.client = client
     }
     
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case connectivity
     }
 }
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
