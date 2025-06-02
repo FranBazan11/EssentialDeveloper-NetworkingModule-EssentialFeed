d87ee3f7f5957c21b65187a750f6550900787fa6
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2025-06-02 13:57:49 -0300

    Move HTTPClient to its own file

---
+++ EssentialFeed/EssentialFeed/FeedAPI/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Juan Francisco Bazan Carrizo on 02/06/2025.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
