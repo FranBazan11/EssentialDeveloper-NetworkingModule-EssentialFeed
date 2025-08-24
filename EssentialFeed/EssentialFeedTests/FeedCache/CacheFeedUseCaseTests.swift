516337f12a7f19f15c5d2e0b7d697cdd8d70f86d
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2025-08-24 01:48:34 -0300

    Does not delete cache upon LocalFeedLoader creation

---
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Juan Francisco Bazan Carrizo on 24/08/2025.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+}
