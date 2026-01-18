c1526ed8e18ee855499c3ba47d531300351b147a
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2026-01-18 17:16:50 -0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Juan Bazan on 18/01/2026.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
